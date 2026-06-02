2941638d9a986b3cfe4b86f27936582dc3d5efbc
venkatapavankumarchillara-06 <chillaravenkatapavankumar06@gmail.com>
2026-06-02 10:20:16 +0530

    Final Updates Code

--- smart_inventory_management/smart_inventory_management/settings.py
+++ smart_inventory_management/smart_inventory_management/settings.py
@@ -61,21 +61,15 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.csrf.CsrfViewMiddleware',
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 
@@ -185,7 +179,7 @@ USE_TZ = True
 
 # STATIC FILES
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -193,6 +187,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # DEFAULT PRIMARY KEY
 
