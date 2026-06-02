26b51edb735ded0c97e10f948ab8b6e1ed424d18
venkatapavankumarchillara-06 <chillaravenkatapavankumar06@gmail.com>
2026-06-02 10:01:53 +0530

    updated code

--- smart_inventory_management/smart_inventory_management/settings.py
+++ smart_inventory_management/smart_inventory_management/settings.py
@@ -183,14 +183,15 @@ USE_TZ = True
 
 # STATIC FILES
 
+# STATIC FILES
+
 STATIC_URL = 'static/'
 
-
 STATICFILES_DIRS = [
-
     os.path.join(BASE_DIR, 'static')
-
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # DEFAULT PRIMARY KEY
